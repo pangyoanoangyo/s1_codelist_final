1e46b7fbb08bf1164f3ec3371fc2a29891e64359
pangyoanoangyo <pil1983@naver.com>
2022-02-25 17:18:50 +0900

    1st final

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,21 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+function getCheckboxValue(){
+    // 선택된 목록 가져오기
+    const query = 'input[name="cam_sel"]:checked';
+    const selectedEls = 
+        document.querySelectorAll(query);
+    
+    // 선택된 목록에서 value 찾기
+    let result = '';
+    selectedEls.forEach((el) => {
+    result += el.value +''+''+'';
+    });
+    
+    // 출력
+    document.getElementById('result').innerText
+    = result;
+}
